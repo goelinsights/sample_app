8fe5ced8858172e488faa2c0bdf22a2b8f6aeecf
Vijay Goel <vjgoel@hotmail.com>
2014-06-01 13:58:15 -0700

    Finished Ch 11

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,25 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer          not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Micropost < ActiveRecord::Base
 	belongs_to :user
 	default_scope -> { order('created_at DESC') }
 	validates :user_id, presence: true
 	validates :content, presence: true, length: { maximum: 140 }
+
+  # Returns microposts from the users being followed by the given user.
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+          user_id: user.id)
+  end
 end
